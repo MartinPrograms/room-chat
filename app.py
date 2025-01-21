a23355c75bbdc601d90623b9360bb7f6e3355bc9
Marten <martinrages@gmail.com>
2025-01-21 16:29:38 +0100

    Simple chat application

---
+++ app.py
@@ -0,0 +1,21 @@
+﻿import os
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET")
+socketio = SocketIO(app)
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template("chat.html")
+
+@socketio.on('message')
+def handle_message(msg):
+    print(f"Message received: {msg}")
+    send(msg, broadcast=True)  # Broadcast the message to all connected clients
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ keygen.py
@@ -0,0 +1,12 @@
+﻿# Keygen.py
+
+import random
+import string
+
+def keygen():
+    key = ''
+    for i in range(64):
+        key += random.choice(string.ascii_letters + string.digits)
+    return key
+
+print(keygen())
